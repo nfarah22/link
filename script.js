e0d25057169987f0298743782b771117261849c6
nfarah22 <61600465+nfarah22@users.noreply.github.com>
2022-08-06 14:18:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-/* paste the JavaScript code from the 'Generated Code' section of the CYF Blocks editor here */
+var list, abr;
+
+
+list = ['http://www.codeyourfuture.io', 'https://twitter.com/?lang=en', 'https://www.youtube.com/'];
+abr = ['CYF', 'Youtube', 'Twitter'];
+let element_list = document.getElementById('list');
+while (!!list.length) {
+  if(--window.LoopTrap <= 0) throw "Infinite loop.";
+  let new_li = document.createElement('li');
+  new_li.setAttribute("href", list.shift());
+  let new_a = document.createElement('a');
+  new_a.innerText = abr.shift();
+
+  new_li.appendChild(new_a);
+
+  element_list.appendChild(new_li);
+}
